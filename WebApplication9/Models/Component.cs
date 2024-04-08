12d04ca4ded7bdfaba160ba38a4ce955add86bed
andryusha2002 <andrey.fedorenko2002@gmail.com>
2024-04-08 19:00:41 +0300

    first commit

---
+++ WebApplication9/Models/Component.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication9.Models
+{
+    public class Component
+    {
+        public int Id { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public int Price { get; set; } = default!;
+    }
+}
---
+++ WebApplication9/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages(options => options.RootDirectory = "/Pages");
+
+var app = builder.Build();
+
+app.MapRazorPages();
+
+app.Run();
